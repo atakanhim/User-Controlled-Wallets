00d508ccf22931a6a08c84b5e4a37bf26c41532d
Atakan Him <52455771+atakanhim@users.noreply.github.com>
2024-07-22 04:27:11 +0300

    init odev 1

---
+++ src/app/api/get_app_id/route.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const get_app_id = async () => {
+  const options = {
+    method: "GET",
+    url: "https://api.circle.com/v1/w3s/config/entity",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
+    },
+  };
+
+  return axios
+    .request(options)
+    .then(function (response) {
+      console.log(response)
+      return response.data.data.appId;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
